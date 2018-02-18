9b65f4ca7a151a54e269177c446a9d48a9e4edde
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-02-19 01:51:32 +0200

    Admin panel - part1

--- lib/router.class.php
+++ lib/router.class.php
@@ -110,4 +110,9 @@ class Router
             $this->params = $path_parts;
         }
     }
+
+    public static function redirect($location)
+    {
+        header("Location: $location");
+    }
 }
